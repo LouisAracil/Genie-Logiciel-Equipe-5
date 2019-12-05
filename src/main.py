95168b7390c6b719b85c481e9924200051fea777
Aracil Louis <louis.aracil@alumni.univ-avignon.fr>
2019-12-05 10:43:55 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,31 +6,22 @@ import glob;
 import re;
 import sys;
 from Parser import *;
-
+ 
 path = sys.argv[2]
-# file = "../../Papers/Papers/Mikolov_2013_Distributed\ representations\ of\ words\ and\ phrases\ and\ their\ compositionality.pdf"
-parser = Parser()
+option = sys.argv[1]
 
-for file in glob.glob(path + '*.pdf'):
+parser = Parser()	
+
+if option == '-t':
+	for file in glob.glob(path + '*.pdf'):
+		print(file)
+		print(parser.parseTxt(file))
+	
+elif option == '-x':
+	for file in glob.glob(path + '*.pdf'):
+		print(parser.parseXML(file))
+
+else :
+	print("error")
 
 
-
-# file = parser.extractText(file);
-# for line in file:
-# 	print("\033[33m{}\033[0m".format(line))
-		
-# txt = parser.parse(file)
-
-# print(
-# 	"Nom du fichier :\n\t", 
-# 	txt["name"],
-# 	"\nTitre :\n\t", 
-# 	txt["title"],
-# 	"\nAbstract :\n", 
-# 	"\t" + txt["abstract"][0] + "\n\t".join(txt["abstract"][1:]),
-# 	end="\n\n")
-	print("\n\n################################################################################\n################################################################################\n################################################################################\n\n")
-	print(parser.parseTxt(file))
-	print("\n--------------------------------------------------------------------------------\n")
-	print(parser.parseXML(file))
-	print("\n\n################################################################################\n################################################################################\n################################################################################\n\n")
