f2877f0d6d7ea0fa20ccc96d0d3a340c5f687dff
Ilona Fresu <ilona.fresu@alumni.univ-avignon.fr>
2019-12-05 10:52:49 +0100

    modification fonction auteur

--- src/Parser.py
+++ src/Parser.py
@@ -81,21 +81,21 @@ class Parser:
 		return [result.strip()]
 
 	def author(self,content):
-	fullTitle = self.title(content)
-	authors = ""
-	successiveLineFeed = 0
-	partialAuthor = self.__fileName.split("_",1)[0]
-	print(fullTitle)
-	for line in content:
-		match = re.search(fullTitle, line)
-		
-		if match != None :
-			span = match.span()
-			authors = line[span[1]:]
-			break
-			if line == "" and authors != "":	# Saut de ligne, donc, fin du titre.
-				return authors.strip()
+		fullTitle = self.title(content)
+		authors = ""
+		successiveLineFeed = 0
+		partialAuthor = self.__fileName.split("_",1)[0]
+		print(fullTitle)
+		for line in content:
+			match = re.search(fullTitle, line)
+			
+			if match != None :
+				span = match.span()
+				authors = line[span[1]:]
 				break
+				if line == "" and authors != "":	# Saut de ligne, donc, fin du titre.
+					return authors.strip()
+					break
 	return partialAuthor
 
 	def __del__(self):
